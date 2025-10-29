3d9f239219caffcac4b9d997cca647c70cfecd97
Thabet <bali.thabet@gmail.com>
2025-10-29 18:13:14 +0100

    testing 1

--- src/StrCalculate.py
+++ src/StrCalculate.py
@@ -1,2 +1,4 @@
 def StrCalculate(number: str):
+    if number == "1":
+        return 1
     return 0
--- tests/test_StrCalculate.py
+++ tests/test_StrCalculate.py
@@ -1,6 +1,6 @@
 import pytest
 from src.StrCalculate import StrCalculate
-@pytest.mark.parametrize("string, result_expected", [("",0), ("0",0)])
+@pytest.mark.parametrize("string, result_expected", [("",0), ("0",0), ("1",1)])
 
 def test(string, result_expected):
     actual_result = StrCalculate(string)
