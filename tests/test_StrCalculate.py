7b71eb38e10662ef8f4950b0f8f63074fff80228
Thabet <bali.thabet@gmail.com>
2025-10-29 17:55:20 +0100

    testing 0

--- tests/test_StrCalculate.py
+++ tests/test_StrCalculate.py
@@ -1,8 +1,7 @@
+import pytest
 from src.StrCalculate import StrCalculate
+@pytest.mark.parametrize("string, result_expected", [("",0), ("0",0)])
 
-
-def test_StrCalculate_function():
-    StrCalculate("")
-
-def test_empty_string():
-    assert StrCalculate("")==0
+def test(string, result_expected):
+    actual_result = StrCalculate(string)
+    assert actual_result == result_expected
