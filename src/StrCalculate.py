b4f8f23ad4b222829a28be32a4ef9d33866afad9
Thabet <bali.thabet@gmail.com>
2025-10-29 18:31:16 +0100

    testing addition of many numbers

--- src/StrCalculate.py
+++ src/StrCalculate.py
@@ -1,8 +1,8 @@
 def StrCalculate(number: str):
-    if number == "1":
-        return 1
-    if number == "2" or number == "1,1" or number == "0,2":
-        return 2
-    if number == "3" or number == "2,1" or number == "1,2" or number == "0,3":
-        return 3
-    return 0
+    if number == "":
+        return 0
+    digits = number.split(",")
+    sum = 0
+    for digit in digits:
+        sum += int(digit)
+    return sum
