22e18b2c234451ed4f308824b7ed90b1ee5ac27f
Thabet <bali.thabet@gmail.com>
2025-10-29 18:24:56 +0100

    testing addition of 1, 2 and 3

--- src/StrCalculate.py
+++ src/StrCalculate.py
@@ -1,6 +1,8 @@
 def StrCalculate(number: str):
     if number == "1":
         return 1
-    if number == "2" or number == "1,1":
+    if number == "2" or number == "1,1" or number == "0,2":
         return 2
+    if number == "3" or number == "2,1" or number == "1,2" or number == "0,3":
+        return 3
     return 0
